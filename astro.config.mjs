90da783a11bab58853cd7edefb7cb017f8dc7441
zoyron <sgrarora3@gmail.com>
2024-10-05 15:16:27 +0530

    updated the site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import solidJs from "@astrojs/solid-js"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.sagar.wtf",
+  site: "https://www.sarlloc.xyz",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 })
